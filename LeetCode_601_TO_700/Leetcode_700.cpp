bac2605a5bd2a8c6935fa84ccea1ed87e888450c
sathvaraneel52@gmail.com <sathvaraneel52@gmail.com>
2024-09-04 23:13:33 +0530

    "Leetcode_700"

---
+++ LeetCode_601_TO_700/Leetcode_700.cpp
@@ -0,0 +1,17 @@
+//* Question 700 - Search in a Binary Search Tree
+
+//! Method - 1
+//? Time Complexity - O(height) or O(log n)  
+//? Space Complexity - O(height) or O(log n)
+
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if(root==NULL or root->val==val) return root;
+
+        else if(val < root->val) return searchBST(root->left,val);
+
+        else return searchBST(root->right,val);
+    }
+};
