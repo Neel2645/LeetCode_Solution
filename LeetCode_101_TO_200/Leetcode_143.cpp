da9ee696b7e9ed0f6395691dc38cb55938b68803
sathvaraneel52@gmail.com <sathvaraneel52@gmail.com>
2024-09-09 01:56:49 +0530

    "Changes"

--- LeetCode_101_TO_200/Leetcode_143.cpp
+++ LeetCode_101_TO_200/Leetcode_143.cpp
@@ -131,3 +131,37 @@ public:
      temp1->next = NULL;
     }
 };
+
+
+
+
+//! Method - 4
+//? Time  Complexity  - O(n)
+//? Space Complexity  - O(n)
+
+class Solution {
+public:
+    ListNode* curr;
+    void solve(ListNode* head)
+    {
+        if(head==NULL) return;
+
+        solve(head->next);
+
+        ListNode* temp = curr->next;
+        if(curr->next==NULL) return;
+        else if(head==curr)
+        {
+            head->next = NULL;
+            return;
+        }
+
+        curr->next = head;
+        head->next = (temp==head)?NULL:temp;
+        curr = temp;
+    }
+    void reorderList(ListNode* head) {
+        curr = head;
+        solve(head);
+    }
+};
