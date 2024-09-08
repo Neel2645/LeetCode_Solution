081da8cccc0b37ba787e076802d1a950e5734ce2
sathvaraneel52@gmail.com <sathvaraneel52@gmail.com>
2024-09-08 23:37:51 +0530

    "Leetcode_143"

---
+++ LeetCode_101_TO_200/Leetcode_143.cpp
@@ -0,0 +1,133 @@
+//* Question 143 ---> Reorder List
+
+//! Method - 1
+//? Time  Complexity  - O(n)
+//? Space Complexity  - O(1)
+
+class Solution {
+public:
+    
+    ListNode* reversell(ListNode* head)
+    {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+
+        while(curr)
+        {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast and fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = reversell(slow->next);
+        slow->next = NULL;
+        ListNode* first = head;
+
+        while(second)
+        {
+            ListNode* temp1 = first->next;
+            first->next = second;
+            ListNode* temp2 = second->next;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
+
+
+//! Method - 2
+//? Time  Complexity  - O(n)
+//? Space Complexity  - O(n)
+
+
+class Solution {
+public:
+    
+    ListNode* reversell(ListNode* head)
+    {
+       if(head==NULL or head->next==NULL) return head;
+
+       ListNode* newHead = reversell(head->next);
+       head->next->next = head;
+       head->next = NULL;
+       return newHead;
+    }
+
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast and fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* second = reversell(slow->next);
+        slow->next = NULL;
+        ListNode* first = head;
+
+        while(second)
+        {
+            ListNode* temp1 = first->next;
+            first->next = second;
+            ListNode* temp2 = second->next;
+            second->next = temp1;
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
+
+
+//! Method - 3
+//? Time  Complexity  - O(n)
+//? Space Complexity  - O(n)
+
+
+class Solution {
+public:
+    
+    void reorderList(ListNode* head) {
+      
+      stack<ListNode*> st;
+      ListNode* temp1 = head;
+
+      while(temp1)
+      {
+        st.push(temp1);
+        temp1 = temp1->next;
+      }
+
+      temp1 = head;
+      ListNode* temp2 = head;
+      ListNode* temp3 = head;
+      int k = st.size()/2;
+
+      while(k--)
+      {
+         temp2 = temp1->next;
+         temp3 = st.top();
+         st.pop();
+         temp1->next = temp3;
+         temp3->next = temp2;
+         temp1 = temp2;
+      }
+     temp1->next = NULL;
+    }
+};
