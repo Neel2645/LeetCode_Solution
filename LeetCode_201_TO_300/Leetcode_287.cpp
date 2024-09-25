ffc1b1b3121070d8f8b5e200fb0efa107c8c7ccb
sathvaraneel52@gmail.com <sathvaraneel52@gmail.com>
2024-09-25 14:31:35 +0530

    "date_25"

--- LeetCode_201_TO_300/Leetcode_287.cpp
+++ LeetCode_201_TO_300/Leetcode_287.cpp
@@ -17,3 +17,23 @@ public:
         return -1;
     }
 };
+
+
+
+//! Method - 2
+//? Time Complexity - O(n logn)
+//? Space Complexity - O(1)
+
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+
+        for(int i=0;i<(arr.size()-1);i++)
+        {
+            if(arr[i]==arr[i+1]) return arr[i];
+        }
+        return -1;
+    }
+};
