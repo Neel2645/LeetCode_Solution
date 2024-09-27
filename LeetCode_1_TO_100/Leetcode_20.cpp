756e6754914595a147a9f4ce18230a61efcf9d66
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-09-27 19:44:05 +0530

    Create Leetcode_20.cpp

---
+++ LeetCode_1_TO_100/Leetcode_20.cpp
@@ -0,0 +1,25 @@
+// Question - 20 ---> valid parentheses 
+// Time  Complexity - O(n)
+// Space Complexity - O(n)
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='{' || s[i]=='[')st.push(s[i]);
+
+            else
+            {
+                if(s[i]==')' && st.size()!=0 &&st.top()=='(')st.pop();
+                else if(s[i]==']' && st.size()!=0 && st.top()=='[')st.pop();
+                else if(s[i]=='}' && st.size()!=0 && st.top()=='{')st.pop();
+                else return false;
+            }
+        }
+        if(!st.empty())return false;
+
+       return true;
+    }
+};
