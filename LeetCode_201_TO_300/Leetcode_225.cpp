bc7461c957528222e8d73c53386756220a4ea3f7
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-09-30 21:37:43 +0530

    "Upd_225"

--- LeetCode_201_TO_300/Leetcode_225.cpp
+++ LeetCode_201_TO_300/Leetcode_225.cpp
@@ -85,3 +85,51 @@ public:
 };
 
 
+
+//! Method - 3
+//? Time Complexity - O(1) For Push,Empty and O(n) for pop,top
+//? Space Complexity - O(n) 
+
+class MyStack {
+public:
+
+    queue<int> q1;
+    queue<int> q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        q1.push(x);
+    }
+    
+    int pop() {
+        while(q1.size()!=1) 
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int x = q1.front();
+        q1.pop();
+        swap(q1,q2);
+        return x;
+    }
+    
+    int top() {
+        while(q1.size()!=1) 
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        int x = q1.front();
+        q1.pop();
+        q2.push(x);
+        swap(q1,q2);
+        return x;
+    }
+    
+    bool empty() {
+        return q1.size()==0;
+    }
+};
