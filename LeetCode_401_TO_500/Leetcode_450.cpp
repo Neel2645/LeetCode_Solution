cd1aa919b3b7d1c30f92eda2d919c15339193a71
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-10-24 21:46:09 +0530

    "update_450"

--- LeetCode_401_TO_500/Leetcode_450.cpp
+++ LeetCode_401_TO_500/Leetcode_450.cpp
@@ -54,3 +54,51 @@ public:
         return dummy;
     }
 };
+
+//! Method - 2
+//? Time Complexity - O(height of tree)  
+//? Space Complexity - O(1)  
+
+class Solution {
+public:
+    TreeNode* predecessor(TreeNode* root)
+    {
+        if(root->left==NULL) return NULL;
+        TreeNode* pred = root->left;
+        while(pred->right) pred = pred->right;
+        return pred;
+    }
+    TreeNode* successor(TreeNode* root)
+    {
+        if(root->right==NULL) return NULL;
+        TreeNode* succ = root->right;
+        while(succ->left) succ = succ->left;
+        return succ;
+    }
+
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return NULL;
+        if(root->val == key)
+        {
+            if(root->left==NULL and root->right==NULL) return NULL;
+
+            else if(root->left==NULL or root->right==NULL)
+            {
+                if(root->left==NULL) return root->right;
+                else return root->left;
+            }
+            else
+            {
+                // TreeNode* pred = predecessor(root);
+                // root->val = pred->val;
+                // root->left = deleteNode(root->left,pred->val);
+                TreeNode* succ = successor(root);
+                root->val = succ->val;
+                root->right = deleteNode(root->right,succ->val);
+            }
+        }
+        else if(key < root->val) root->left = deleteNode(root->left,key);
+        else root->right = deleteNode(root->right,key);
+        return root;
+    }
+};
