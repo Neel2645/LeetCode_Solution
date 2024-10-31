5b6423b16891448f1e0112aed24a897b40388531
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-10-31 23:27:03 +0530

    "UPDATE_1464"

--- LeetCode_1401_TO_1500/Leetcode_1464.cpp
+++ LeetCode_1401_TO_1500/Leetcode_1464.cpp
@@ -12,3 +12,32 @@ public:
         return (nums[0]-1)*(nums[1]-1);
     }
 };
+
+
+//! Method - 2
+//? Time Complexity - O(n)
+//? Space Complexity - O(1)
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int max1 = 0;
+        int max2 = 0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[max2]<=nums[i])
+            {
+                max2 = i;
+            }
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(max1<=nums[i] and i!=max2)
+            {
+                max1 = nums[i];
+            }
+        }
+        return (max1-1)*(nums[max2]-1);
+    }
+};
