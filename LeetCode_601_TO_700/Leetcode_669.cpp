d678950f01488ab56eca9d1866278255b41ad301
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-11-04 22:20:26 +0530

    "update_669"

--- LeetCode_601_TO_700/Leetcode_669.cpp
+++ LeetCode_601_TO_700/Leetcode_669.cpp
@@ -33,3 +33,34 @@ public:
         return trim(root,low,high);
     }
 };
+
+
+//! Method - 2
+//? Time Complexity - O(n)
+//? Space Complexity - O(n)
+
+
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root==NULL) return root;
+
+        else if(root->val >= low and root->val<=high)
+        {
+            root->left = trimBST(root->left,low,high);
+            root->right = trimBST(root->right,low,high);
+            return root;
+        }
+
+        else if(root->val > high)
+        {
+            return trimBST(root->left,low,high);
+        }
+
+        else if(root->val < low)
+        {
+            return trimBST(root->right,low,high);
+        }
+        return root;
+    }
+};
