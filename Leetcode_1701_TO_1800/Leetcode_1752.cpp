05370e7bb7666a7f572d1253b7c7609ee0c7f81d
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-11-06 22:39:26 +0530

    'UPDATE_1752"

--- Leetcode_1701_TO_1800/Leetcode_1752.cpp
+++ Leetcode_1701_TO_1800/Leetcode_1752.cpp
@@ -20,3 +20,24 @@ public:
         return cnt<=1;
     }
 };
+
+
+//! Method - 2
+//? Time Complexity - O(n^2 * log n)
+//? Space Complexity - O(n)
+
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        vector<int> ans(nums.begin(), nums.end());
+        sort(ans.begin(), ans.end());
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums == ans) {
+                return true;
+            }
+            rotate(nums.begin(), nums.begin() + 1, nums.end());
+        }
+        return false;
+    }
+};
