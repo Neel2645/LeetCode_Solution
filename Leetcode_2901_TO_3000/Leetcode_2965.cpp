00370fb104549618f8dfd21ca3248ac14def6226
Neel_Sathvara <sathvaraneel52@gmail.com>
2024-11-14 23:05:00 +0530

    "Leetcode_2965"

--- Leetcode_2901_TO_3000/Leetcode_2965.cpp
+++ Leetcode_2901_TO_3000/Leetcode_2965.cpp
@@ -31,3 +31,27 @@ public:
         return ans;
     }
 };
+
+
+//! Method - 2
+//? Time Complexity - O(n^2)
+//? Space Complexity - O(n^2) 
+
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int>vec(n*n +1,0);
+        for(auto & it: grid){
+            for(auto & i: it){
+                vec[i]++;
+            }
+        }
+        int a,b;
+        for(int i=1;i<=n*n;i++){
+            if(vec[i]==2) a = i;
+            if(vec[i]==0) b = i;
+        }
+        return {a,b};
+    }
+};
